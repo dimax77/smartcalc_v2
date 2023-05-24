981b4d6df10ff5f740ffff7d481f8eb649f8ba95
mockingb <m.1@bk.ru>
2023-05-24 09:23:17 +0300

    а

--- src/model/parser.h
+++ src/model/parser.h
@@ -1,25 +1,31 @@
+// Function aliases:
+// c: cos   a: acos   l: ln       m: mod
+// s: sin   i: asin   o: log      q: sqrt
+// t: tan   v: atan   u: unar '-' ^: pow
+
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
+#include <cstring>
 #include <list>
 #include <stack>
 #include <string>
 
 namespace s21 {
+
 class ConvertToPostfix {
   std::string in_{};
   std::list<std::string> output_tokens_{};
-  std::string accessible_tokens_after_digit_ = "-+*/)m^";
-  std::string postfix_string_;
-  std::stack<char> operators_;
+  std::string acceptable_tokens_after_digit_ = "-+*/)m^";
+  std::string acceptable_tokens_after_op_ = "x0123456789cstaivloq(";
+  std::string postfix_string_{};
+  std::stack<char> operators_{};
 
-  inline bool is_num(char c) {
-    return (c >= '0' && c <= '9') || c == '.' || c == 'x';
-  }
-  inline bool is_func(char c) { return strchr("-+*/^cstaivlomq", c); }
-   void move_operator_to_output_tokens_() {
-    // output_tokens_.push_back(std::string(1, operators_.top()));
-    std::cout<< "Pushed - " << operators_.top() << std::endl;
+  inline bool is_func(char c) { return strchr("cstaivloq", c); }
+  inline bool is_operator(char c) { return strchr("-+*/^m", c); }
+
+  void move_operator_to_output_tokens() {
+    output_tokens_.push_back(std::string(1, operators_.top()));
     operators_.pop();
   }
 
@@ -30,73 +36,116 @@ class ConvertToPostfix {
       return 2;
     } else if (op == '^') {
       return 3;
-    } else if (op == 'c' || op == 's' || op == 't' || op == 'a' || op == 'i' ||
-               op == 'v' || op == 'q') {
+    } else if (is_func(op)) {
       return 4;
     } else {
       return 0;
     }
   }
 
+  void processNumber(std::size_t& idx, std::size_t& temp_idx, bool& no_num,
+                     std::size_t size) {
+    double n = std::stod(in_.substr(idx), &temp_idx);
+    output_tokens_.push_back(std::to_string(n));
+    no_num = false;
+    idx += (temp_idx - 1);
+    if (idx + 1 < size) {
+      if (acceptable_tokens_after_digit_.find(in_[idx + 1]) ==
+          std::string::npos) {
+        throw std::runtime_error("Malformed expression.");
+      }
+    }
+  }
+
+  void processFunction(char c, std::size_t idx, bool& no_num) {
+    if (isdigit(in_[idx + 1]) || in_[idx + 1] == '(') {
+      operators_.push(c);
+      no_num = true;
+    } else {
+      throw std::runtime_error("Malformed expression.");
+    }
+  }
+
+  void processOperator(char c, std::size_t idx, bool& no_num) {
+    if (c == '-' && no_num) {
+      operators_.push('u');
+    } else {
+      if (acceptable_tokens_after_op_.find(in_[idx + 1]) == std::string::npos) {
+        throw std::runtime_error("Malformed expression.");
+      } else {
+        while (precedence(c) <= precedence(operators_.top())) {
+          move_operator_to_output_tokens();
+        }
+        operators_.push(c);
+        no_num = true;
+      }
+    }
+  }
+
+  void processOpenBrace(std::size_t idx) {
+    if (idx > 0 && (isdigit(in_[idx - 1]) || in_[idx - 1] == '.')) {
+      throw std::runtime_error("Malformed expression.");
+    }
+    operators_.push('(');
+  }
+
+  void processCloseBrace() {
+    while (operators_.top() != '(') {
+      move_operator_to_output_tokens();
+      if (operators_.top() == 'S') {
+        throw std::runtime_error("Unmatched braces.");
+      }
+    }
+    operators_.pop();
+  }
+
+  void processVariable() { output_tokens_.push_back("x"); }
+
  public:
   void set_input_string(std::string raw) { in_ = raw; }
+
   void translate() {
-    std::size_t size = in_.length(), idx = 0, temp_idx{};
-    bool status = true, no_num = true;
-    std::stack<char> operators{};
-    operators.push('S');
-    std::cout << size << std::endl;
+    std::size_t size = in_.length(), idx{}, temp_idx{};
+    bool no_num = true;
+    operators_.push('S');
     while (idx < size) {
       char c = in_[idx];
-      if (is_num(c)) {  // Number
-        std::cout << idx << std::endl;
-        double n = std::stod(in_.substr(idx), &temp_idx);
-        output_tokens_.push_back(std::to_string(n));
+      if (isdigit(c)) {
+        processNumber(idx, temp_idx, no_num, size);
+      } else if (is_func(c)) {
+        processFunction(c, idx, no_num);
+      } else if (is_operator(c)) {
+        processOperator(c, idx, no_num);
+      } else if (c == '(') {
+        processOpenBrace(idx);
+      } else if (c == ')') {
+        processCloseBrace();
+      } else if (c == 'x') {
+        processVariable();
         no_num = false;
-        idx += (temp_idx - 1);
-        // if (!strchr(accessible_tokens_after_digit_, in_[idx])) {
-        //   throw("Malformed expression.");
-        // }
-      } else if (is_func(c)) {  // Operator
-        if (c == '-' && no_num) {
-          operators.push('u');
-        } else {
-          while (precedence(c) <= precedence(operators.top())) {
-            move_operator_to_output_tokens_();
-          }
-          operators.push(c);
-          std::cout << c << std::endl;
-          no_num = true;
-        }
-      } else if (c == '(') {  // Open brace
-        if (is_num(in_[idx - 1])) throw "Error";
-        operators.push(c);
-      } else if (c == ')') {  // Close brace
-        while (operators.top() != '(') {
-          move_operator_to_output_tokens_();
-        }
-        operators.pop();  // drop the '('
+      } else {
+        throw std::runtime_error("Unknown symbol");
       }
       idx += 1;
     }
-    while (operators.top() != 'S') {
-                  std::cout << operators.top() << std::endl;
-
-        move_operator_to_output_tokens_();
+    while (operators_.top() != 'S') {
+      if (operators_.top() == '(')
+        throw std::runtime_error("Unmatched braces.");
+      move_operator_to_output_tokens();
     }
-    // operators.pop();
+    operators_.pop();
   }
+
   std::string postfix_string() {
-    std::cout << "Start" << std::endl;
     while (!output_tokens_.empty()) {
       postfix_string_ += output_tokens_.front();
+      postfix_string_ += " ";
       output_tokens_.pop_front();
     }
-    std::cout << "End" << std::endl;
-
     return postfix_string_;
   }
 };
+
 }  // namespace s21
 
 #endif  // _PARSER_H_
--- src/model/test.cc
+++ src/model/test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "parser.h"
 
 TEST(ConvertToPostfixTest, EmptyInputString) {
@@ -12,24 +13,110 @@ TEST(ConvertToPostfixTest, SingleNumber) {
   s21::ConvertToPostfix converter;
   converter.set_input_string("123.45");
   converter.translate();
-  EXPECT_EQ(converter.postfix_string(), "123.45");
+  EXPECT_EQ(converter.postfix_string(), "123.450000 ");
+}
+
+TEST(ConvertToPostfixTest, SingleNumberWithTrailingZeroes) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("12.500");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "12.500000 ");
+}
+
+TEST(ConvertToPostfixTest, IncorrectExpressionMultipleDecimals) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("12.5.0");
+  ASSERT_THROW(converter.translate(), std::runtime_error);
 }
 
 TEST(ConvertToPostfixTest, SimpleExpression) {
   s21::ConvertToPostfix converter;
   converter.set_input_string("2+3*4");
   converter.translate();
-  EXPECT_EQ(converter.postfix_string(), "234*+");
+  EXPECT_EQ(converter.postfix_string(), "2.000000 3.000000 4.000000 * + ");
 }
 
 TEST(ConvertToPostfixTest, ComplexExpression) {
   s21::ConvertToPostfix converter;
-  converter.set_input_string("(3 + 4) * (2 - 1) / 5");
+  converter.set_input_string("(3+4)*(2-1)/5");
   converter.translate();
-  EXPECT_EQ(converter.postfix_string(), "3 4 + 2 1 - * 5 /");
+  EXPECT_EQ(converter.postfix_string(), "3.000000 4.000000 + 2.000000 1.000000 - * 5.000000 / ");
 }
 
-// Добавьте больше тестов, покрывающих различные сценарии и граничные случаи
+TEST(ConvertToPostfixTest, ComplexExpression2) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(3+4)*(2-1)/5/");
+  ASSERT_THROW(converter.translate(), std::runtime_error);
+}
+
+TEST(ConvertToPostfixTest, NestedBracketsDepth1) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("((1+2)*3)/4");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "1.000000 2.000000 + 3.000000 * 4.000000 / ");
+}
+
+TEST(ConvertToPostfixTest, NestedBracketsDepth2) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(((1+2)*3)-4)/5");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "1.000000 2.000000 + 3.000000 * 4.000000 - 5.000000 / ");
+}
+
+TEST(ConvertToPostfixTest, NestedBracketsDepth3) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(((1+2)*3)-4)/(5+6)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "1.000000 2.000000 + 3.000000 * 4.000000 - 5.000000 6.000000 + / ");
+}
+
+TEST(ConvertToPostfixTest, IncorrectExpressionMissingClosingBracket) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(1+2*3");
+  ASSERT_THROW(converter.translate(), std::runtime_error);
+}
+
+TEST(ConvertToPostfixTest, IncorrectExpressionExtraClosingBracket) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(1+2)*3)");
+  ASSERT_THROW(converter.translate(), std::runtime_error);
+}
+
+TEST(ConvertToPostfixTest, ComplexExpressionWithCos) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("c(x)+s(x)*t(x)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "x c x s x t * + ");
+}
+
+TEST(ConvertToPostfixTest, ComplexExpressionWithAcos) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("a(x)-i(x)/v(x)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "x a x i x v / - ");
+}
+
+TEST(ConvertToPostfixTest, ComplexExpressionWithLn) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("l(x)*o(x)/q(x)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "x l x o * x q / ");
+}
+
+TEST(ConvertToPostfixTest, ComplexExpressionWithPow) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(x^2)+(2^x)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "x 2.000000 ^ 2.000000 x ^ + ");
+}
+
+TEST(ConvertToPostfixTest, ComplexExpressionWithMod) {
+  s21::ConvertToPostfix converter;
+  converter.set_input_string("(xm3)+(3mx)");
+  converter.translate();
+  EXPECT_EQ(converter.postfix_string(), "x 3.000000 m 3.000000 x m + ");
+}
+
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
